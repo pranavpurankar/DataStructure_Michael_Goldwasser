1741f33f954e9da2bdfc048ce591491464fdd95f
pranavpurankar <pranav.purankar@gmail.com>
2024-06-14 11:18:54 +0530

    Square and Sum of all odd

---
+++ 01.Chapter_Python-Primer/R_1.6.squares_odd.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+'''
+Write a short Python function that takes a positive integer n and return the
+sum of the squares of all the odd positive integers smaller than n.
+'''
+
+
+def square_sum_odd(n):
+    if n <= 0:
+        raise ValueError("n should be positive integer")
+    sum_of_squares = 0
+    for num in range(1, n, 2):
+        sum_of_squares = sum_of_squares + (num * num)
+    return sum_of_squares
+
+
+print(square_sum_odd(6))
+
+
+# Another approach is using list comprehension, it is more efficient and avoid
+# unnecessary conditionals as well as calculations
+
+def square_odd(n):
+    if n <= 0:
+        raise ValueError("n should be positive integer")
+
+    square_num = [num**2 for num in range(1, n, 2)]
+    return sum(square_num)
+
+
+print(square_odd(6))
