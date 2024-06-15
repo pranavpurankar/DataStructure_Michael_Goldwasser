12f13c8c29558c7a2903bb86e05443cf987dc424
pranavpurankar <pranav.purankar@gmail.com>
2024-06-15 07:57:31 +0530

    Defined a custom range

--- 01.Chapter_Python-Primer/Exercise_Files/R_1.9.range_constructor.py
+++ 01.Chapter_Python-Primer/Exercise_Files/R_1.9.range_constructor.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 '''
-What parameter should be sent to the range constructor, to produce a range 
+What parameter should be sent to the range constructor, to produce a range
 with values 50, 60, 70, 80?
 '''
 
 
-def range
+for num in range(50, 81, 10):
+    print(num)
