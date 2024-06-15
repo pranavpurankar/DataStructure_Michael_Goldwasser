51c85799ae4b4d89f0d9d4193411af322b5b05b7
pranavpurankar <pranav.purankar@gmail.com>
2024-06-15 10:00:49 +0530

    Changed file names, wrong bash script

---
+++ 01.Chapter_Python-Primer/Exercise_Files/R_01.2.is_even.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+'''
+Reinforcement 1.2
+Write a short Python function, is_even(k), that takes an integer value and
+returns True if k is even, and False otherwise. However, your function cannot
+use the multiplication, modulo, or division operator.
+'''
+
+
+def is_even(k):
+    if k in for eve in range(0, 10, 2):
+        return True
+    else:
+        return False
+
+
+print(is_even(6))
---
+++ 01.Chapter_Python-Primer/Exercise_Files/R_01.9.range_constructor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+Reinforcement 1.9
+What parameter should be sent to the range constructor, to produce a range
+with values 50, 60, 70, 80?
+'''
+
+
+for num in range(50, 81, 10):
+    print(num)
