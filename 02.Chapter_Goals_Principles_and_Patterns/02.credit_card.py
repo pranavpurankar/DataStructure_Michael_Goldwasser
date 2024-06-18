09a64d112890b17a20d50ee7cba712a0a121990b
pranavpurankar <pranav.purankar@gmail.com>
2024-06-18 17:20:19 +0530

    Added credit card program with test cases

--- 02.Chapter_Goals_Principles_and_Patterns/02.credit_card.py
+++ 02.Chapter_Goals_Principles_and_Patterns/02.credit_card.py
@@ -28,7 +28,7 @@ class CreditCard:
         '''Return name of the bank'''
         return self._bank
 
-    def get_acount(self):
+    def get_account(self):
         '''Return the card indentifying number (typically store as a string)'''
         return self._account
 
@@ -54,5 +54,30 @@ class CreditCard:
         '''Process customer payment that reduces balance.'''
         self._balance -= amount
 
+
 cc = CreditCard('Pranav Purankar', '1st Bank', '5391 0375 9387 5309', 1000)
 
+
+# Testing the Credit Card class
+
+if __name__ == '__main__':
+    wallet = []
+    wallet.append(CreditCard('John Bowman', 'California Savings', '5391 0375 9387 5309', 2500))
+    wallet.append(CreditCard('John Bowman', 'California Federal', '5391 0375 9387 5309', 3500))
+    wallet.append(CreditCard('John Bowman', 'California Finance', '5391 0375 9387 5309', 5000))
+
+    for val in range(1, 17):
+        wallet[0].charge(val)
+        wallet[1].charge(2 * val)
+        wallet[2].charge(3 * val)
+
+    for c in range(3):
+        print('Customer =', wallet[c].get_customer())
+        print('Bank =', wallet[c].get_bank())
+        print('Account =', wallet[c].get_account())
+        print('Limit =', wallet[c].get_limit())
+        print('Balance =', wallet[c].get_balance())
+        while wallet[c].get_balance() > 100:
+            wallet[c].make_payment(100)
+            print('New balance =', wallet[c].get_balance())
+        print()
