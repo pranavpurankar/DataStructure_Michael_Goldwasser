28e1cce91eccce6b204a67794e4851c6935be254
pranavpurankar <pranav.purankar@gmail.com>
2024-07-06 10:51:07 +0530

    chapter 2, more code added

---
+++ 02.Chapter_Goals_Principles_and_Patterns/lessons/practice.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# This is the base class
+
+class Progression:
+
+    def __init__(self, start=0):
+        self._current = start
+
+    def _advance(self):
+        self._current += 1
+
+    def __next__(self):
+        if self._current is None:
+            raise StopIteration()
+        else:
+            answer = self._current
+            self._advance()
+            return answer
+
+    def __iter__(self):
+        return self
+
+    def print_progression(self, n):
+        print(' '.join(str(next(self) for j in range(n))))
+
+
+p1 = Progression(8)
+print(p1.print_progression(8))
